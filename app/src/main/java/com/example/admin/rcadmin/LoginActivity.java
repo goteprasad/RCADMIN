91defcef547d6179670de9d83300ee17b07f05ed
Prasad Gote <gote.prasad@yahoo.in>
2018-01-17 22:40:55 +0530

    Enquiry details UI

--- app/src/main/java/com/example/admin/rcadmin/LoginActivity.java
+++ app/src/main/java/com/example/admin/rcadmin/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.example.admin.rcadmin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnlogin = (Button) findViewById(R.id.loginbtn);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
